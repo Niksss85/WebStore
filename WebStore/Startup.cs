c47ce2efee726454dbec70cc48d5f6ad0ce4cc4d
Infarh <shmachilin@yandex.ru>
2021-01-15 20:31:55 +0300

    Динамическое конфигурирование приложения

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace WebStore
 {
-    public class Startup
+    public record Startup(IConfiguration Configuration)
     {
+        //private IConfiguration Configuration { get; }
+
+        //public Startup(IConfiguration Configuration)
+        //{
+        //    this.Configuration = Configuration;
+        //}
+        
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -21,11 +29,13 @@ namespace WebStore
 
             app.UseRouting();
 
+            //var greetings = Configuration["Greetings"];
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    //await context.Response.WriteAsync(greetings);
+                    await context.Response.WriteAsync(Configuration["Greetings"]);
                 });
             });
         }
