acd222eacf6f7ca1b1800ec054783506acba55c5
Nik Pav <kol-pav@mail.ru>
2021-01-23 14:53:46 +0300

    Добавление разметки страниц

---
+++ WebStore/Controllers/BlogSingleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class BlogSingleController : Controller
+    {
+
+
+        public IActionResult Index() => View();
+
+
+
+    }
+}
---
+++ WebStore/Controllers/ProductDetailsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+
+
+        public IActionResult Index() => View();
+
+
+
+    }
+}
