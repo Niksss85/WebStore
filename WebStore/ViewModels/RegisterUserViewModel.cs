7b1020d241afbc1812cd518287e9fb88cdcfdf58
Nikolay <kol-pav@mail.ru>
2021-02-04 16:27:37 +0300

    Регистрация пользователя, контроллер Account

---
+++ WebStore/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required, MaxLength(256)]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Password confirmation")]
+
+        public string PasswordConfirm { get; set; }
+    }
+}
