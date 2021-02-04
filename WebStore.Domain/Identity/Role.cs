8d5da20e018a12b32d885146ac4d81ef28c8cc42
Nikolay <kol-pav@mail.ru>
2021-02-04 13:49:17 +0300

    Сущности "пользователь" и "роль"

---
+++ WebStore.Domain/Identity/Role.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Identity
+{
+    public class Role : IdentityRole
+    {
+        public const string Administrtator = "Administrators";
+
+        public const string Users = "Users";
+        public string Description { get; set; }
+    }
+}
