d950d0c7bd27ade619fbf91492b0abb517d7c4cf
Nik Pav <kol-pav@mail.ru>
2021-02-07 16:17:15 +0300

    Разрешение доступа к определенным методам внутри заблокированного контроллера

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,6 +11,7 @@ using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         private readonly UserManager<User> _UserManager;
@@ -24,8 +26,10 @@ namespace WebStore.Controllers
         }
         #region Register
         //отправка Html  с формой
+        [AllowAnonymous]
         public IActionResult Register() => View(new RegisterUserViewModel());
         //принятие формы обратно
+        [AllowAnonymous]
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterUserViewModel Model)
         {
@@ -58,9 +62,11 @@ namespace WebStore.Controllers
         }
         #endregion
         #region Login
+        [AllowAnonymous]
         public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
-
-        [HttpPost, ValidateAntiForgeryToken]
+       
+        [AllowAnonymous]
+        [HttpPost, ValidateAntiForgeryToken] 
         public async Task<IActionResult> Login(LoginViewModel Model)
         {
             if (!ModelState.IsValid) return View(Model);
